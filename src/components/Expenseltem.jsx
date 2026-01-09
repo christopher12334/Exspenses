09afac00b826695f9f8f597442ee944b4a9740e8
christopher12334 <christopher.alapert@voco.ee>
2026-01-09 11:35:13 +0200

    Task 4 done

--- src/components/Expenseltem.jsx
+++ src/components/Expenseltem.jsx
@@ -1,13 +1,11 @@
+import ExpenseDate from './ExspenseDate'
 import './ExpenseItem.css'
 
 const ExpenseItem = (props) => {
 
-    console.log(props)
-    console.log(props.data)
-
-    return(
+   return(
         <div className="expense-item">
-            <div>{props.data.date.toString()}</div>
+            <ExpenseDate date={props.data.date}/>
             <div className='expense-item__description'>
                 <h2>{props.data.title}</h2>
                 <div className='expense-item__price'>{props.data.price}</div>
